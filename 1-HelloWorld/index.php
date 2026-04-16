18a775165921b27145e012b294328fcbb1cf8869
TaigcoutoProf <taigcouto.prof@gmail.com>
2026-04-16 08:30:54 -0300

    First commit: Insere código exemplo inicial de hello world

---
+++ 1-HelloWorld/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP no navegador</title>
+</head>
+<body>
+    <p> <?php echo "Olá mundo!"; ?></p>
+    <p> 20 + 20 é <?php echo 20 + 20 ?> ! </p>
+</body>
+</html>
